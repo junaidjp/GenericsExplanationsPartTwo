47161eb17be23ba4cacf54e9c097683a02491f9f
Naeem <naeemjahangir@gmail.com>
2015-10-31 12:22:22 -0700

    Added my new class by Naeem

---
+++ src/com/training/concepts/NaeemTest.java
@@ -0,0 +1,12 @@
+package com.training.concepts;
+
+public class NaeemTest {
+
+
+		
+		public static void main (String args[]) 
+		{
+			
+			System.out.println("New Class promote it to GIT HUB");
+		}
+	}
