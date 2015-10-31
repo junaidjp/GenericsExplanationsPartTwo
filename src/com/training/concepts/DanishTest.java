fd947ee08a5078c7f17b44abd6cc133d53f9e13b
Danish Ahmed <ahmed50406@gmail.com>
2015-10-31 12:15:35 -0500

    class promote it to GIT HUB

---
+++ src/com/training/concepts/DanishTest.java
@@ -0,0 +1,14 @@
+package com.training.concepts;
+
+public class DanishTest {
+
+	public static void main (String args[]){
+		
+		
+		
+		
+		
+		
+		System.out.println("THE CLAss practic it to GIT HUB");
+	}
+}
